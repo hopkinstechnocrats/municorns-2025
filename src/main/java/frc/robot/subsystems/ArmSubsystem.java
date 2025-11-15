90dabbc8235c5d98d4836ed73992d7dc87dfc21b
lucian <lucian.pier@hopkinsrobotics.org>
2025-11-15 11:15:20 -0600

    added brake to arm sub system

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -8,12 +8,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ArmSubsystem extends SubsystemBase{
     WPI_TalonSRX armMotor;
     public ArmSubsystem(){
-        armMotor = new WPI_TalonSRX(0); // get CANID
+        armMotor = new WPI_TalonSRX(0); //TODO: get CANID
         armMotor.configFactoryDefault();
         armMotor.setNeutralMode(NeutralMode.Brake);
     }
 
     public void motorSpin(double speed){
         armMotor.set(speed);
+    }
+    //TODO:is motor spin =0 the same as brake
+    public void brake(){
+        armMotor.stopMotor();
     }
 }
