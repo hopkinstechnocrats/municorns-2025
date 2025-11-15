e7ffe3a7ce7cf19faa428d30040944a9559714ed
Asher Fix <asher.fix@hopkinsrobotics.org>
2025-11-15 14:06:42 -0600

    actual possibly working auto stuff?

--- src/main/java/frc/robot/Autos/AutoRoutines.java
+++ src/main/java/frc/robot/Autos/AutoRoutines.java
@@ -11,12 +11,12 @@ public class AutoRoutines {
     SendableChooser<Command> autoChooser        = new SendableChooser<>(); 
 
     public Command moveForward(double time) {return DriveCommands.drive(driveSubsystem, 1, 1).withTimeout(1);};
-    public Command rotateClockwise90        = DriveCommands.drive(driveSubsystem, 1, -1).withTimeout(.5);
-    public Command rotateCounterclockwise90 = DriveCommands.drive(driveSubsystem, -1, 1).withTimeout(.5);
-    public Command rotateClockwise45        = DriveCommands.drive(driveSubsystem, .5, -.5).withTimeout(1);
-    public Command rotateCounterclockwise45 = DriveCommands.drive(driveSubsystem, -.5, .5).withTimeout(1);
+    public Command rotateClockwise90        = DriveCommands.drive(driveSubsystem, .1, -.1).withTimeout(.5);
+    public Command rotateCounterclockwise90 = DriveCommands.drive(driveSubsystem, -.1, .1).withTimeout(.5);
+    public Command rotateClockwise45        = DriveCommands.drive(driveSubsystem, .05, -.05).withTimeout(1);
+    public Command rotateCounterclockwise45 = DriveCommands.drive(driveSubsystem, -.05, .05).withTimeout(1);
 
-    public Command drive_to_melter = new SequentialCommandGroup(DriveCommands.drive(driveSubsystem, 1, 1).withTimeout(1), DriveCommands.drive(driveSubsystem, -1, 1).withTimeout(1));
+    public Command drive_to_melter = new SequentialCommandGroup(DriveCommands.drive(driveSubsystem, .1, .1).withTimeout(1), DriveCommands.drive(driveSubsystem, -.1, .1).withTimeout(1));
     public Command do_nothing      = new SequentialCommandGroup(Commands.waitSeconds(15));
 
     public Command selectAuto(){
