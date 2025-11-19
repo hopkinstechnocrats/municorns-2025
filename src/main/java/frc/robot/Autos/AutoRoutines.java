b35baa069db3059c9d7e423bc649465cb57dd08e
Asher Fix <asher.fix@hopkinsrobotics.org>
2025-11-18 20:30:20 -0600

    Added possibly working intake and arm autos once merged with other branches

--- src/main/java/frc/robot/Autos/AutoRoutines.java
+++ src/main/java/frc/robot/Autos/AutoRoutines.java
@@ -5,22 +5,36 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Commands.DriveCommands;
+//import frc.robot.Commands.ArmCommands;
+//import frc.robot.subsystems.ArmSubsystem;
+//import frc.robot.subsystems.IntakeSubsystem;
+//import frc.robot.Commands.IntakeCommands
+
 
 public class AutoRoutines {
     private final DriveSubsystem driveSubsystem = new DriveSubsystem();
     SendableChooser<Command> autoChooser        = new SendableChooser<>(); 
 
-    public Command moveForward(double time) {return DriveCommands.drive(driveSubsystem, 1, 1).withTimeout(1);};
-    public Command rotateClockwise90        = DriveCommands.drive(driveSubsystem, .1, -.1).withTimeout(.5);
-    public Command rotateCounterclockwise90 = DriveCommands.drive(driveSubsystem, -.1, .1).withTimeout(.5);
-    public Command rotateClockwise45        = DriveCommands.drive(driveSubsystem, .05, -.05).withTimeout(1);
-    public Command rotateCounterclockwise45 = DriveCommands.drive(driveSubsystem, -.05, .05).withTimeout(1);
+    public Command moveForward(double driveTime) {return DriveCommands.drive(driveSubsystem, 1, 1).withTimeout(driveTime);};
+    public Command Rotate (double rightSpeed, double leftSpeed) {return DriveCommands.drive(driveSubsystem, rightSpeed, leftSpeed).withTimeout(1);};//Make sure to Callibrate!!!!
+    //public Command armMovement(double armSpeed, double armTime) {return ArmCommands.moveArm(armSubsystem, armSpeed).withTimeout(armTime);};//Make sure to Calibrate!!!
+    //public Command intakeOutake(double intakeSpeed, double intakeTime) {return IntakeCommands.intake(intakeSubsystem, intakeSpeed).withTimeout(intakeTime);};//Make sure to Calibrate!!!
 
-    public Command drive_to_melter = new SequentialCommandGroup(DriveCommands.drive(driveSubsystem, .1, .1).withTimeout(1), DriveCommands.drive(driveSubsystem, -.1, .1).withTimeout(1));
+
+    public Command drive_to_melter = new SequentialCommandGroup(moveForward(1), Rotate(.1, -.1));
     public Command do_nothing      = new SequentialCommandGroup(Commands.waitSeconds(15));
+    //public Command raise_arm       = new SequentialCommandGroup(armMovement(.1, 1));
+    //public Command lower_arm       = new SequentialCommandGroup(armMovement(-.1, 1));
+    //public Command outake          = new SequentialCommandGroup(intakeOutake(.1, 1));
+    //public Command score           = new SequentialCommandGroup(raise_arm, outake);
+    //public Command intake          = new SequentialCommandGroup(lower_arm, armMovement(-.1, 1));
 
     public Command selectAuto(){
         autoChooser.setDefaultOption("Do Nothing", do_nothing);
+        autoChooser.addOption("Drive to Melter", drive_to_melter);
+        //autoChooser.addOption("Score", score);
+        //autoChooser.addOption("Intake", intake);
         return autoChooser.getSelected();
+
     }
 }
